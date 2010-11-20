c764bf936b98b077db4ed9d2ca2f4305209cbf1e
supersnail <supersnail-du-91@hotmail.fr>
2010-11-20 10:30:43 +0100

    add ui file

--- ui_QEditorWindow.h
+++ ui_QEditorWindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'QEditorWindow.ui'
 **
-** Created: Fri Nov 19 18:59:43 2010
+** Created: Sat Nov 20 09:55:27 2010
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
